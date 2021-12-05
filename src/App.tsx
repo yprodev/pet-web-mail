08f622f82f60196f30c9eb65a259cbf3e40bf3a3
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2021-12-05 05:39:42 +0200

    [Task 4] Define Aside component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,10 @@
-import { useState, useEffect } from 'react'
-
-import resourceMail from './resource/email'
-import { Email } from './types'
+import { Aside } from './components'
 
 function App() {
-  const [emails, setEmails] = useState<Array<Email>>([])
-
-  const handlerFetch = async () => {
-    const res = await resourceMail.getAll()
-    setEmails(res)
-    console.log('result', res)
-  }
-
-  useEffect(() => {
-    const data = handlerFetch()
-    console.log('data: --->', data)
-
-    return () => {}
-  }, [])
-
   return (
-    <div className='App'>
-      {emails.map((email: Email, idx: number) => {
-        return <div key={idx}>{email.id}</div>
-      })}
-    </div>
+    <section className='mainScreen'>
+      <Aside />
+    </section>
   )
 }
 
---
+++ src/components/Aside.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+
+interface AsideProps {}
+
+const Aside: FC<AsideProps> = () => {
+  return <aside>Aside element</aside>
+}
+
+export default Aside
